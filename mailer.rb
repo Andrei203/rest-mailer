436158ab0ffa05895837e690b824dddb8965f3f3
Daniel Moulson <dan24797@gmail.com>
2020-10-19 13:40:32 +0100

    change to smtp

--- mailer.rb
+++ mailer.rb
@@ -6,9 +6,20 @@ get '/' do
         Pony.options = {
             subject: "MoulsonMail: #{params[:subject]}",
             body: "#{params[:message]}, From: #{params[:name]} <#{params[:sender]}>",
+            via: :smtp,
+            via_options: {
+                address: ENV['SMTP_ADDRESS'],
+                ssl: true,
+                arguments: '-t',
+                port: ENV['SMTP_PORT'],
+                enable_starttls_auto: true,
+                user_name: ENV['SMTP_USERNAME'],
+                password: ENV['SMTP_PASSWORD'],
+                authentication: :plain,
+                domain: 'restmailer.danmoulson.com'
+            }
         }
         Pony.mail(to: params[:recipient])
         "Sent!"
     end
-    Process.kill('TERM', Process.pid)
 end
